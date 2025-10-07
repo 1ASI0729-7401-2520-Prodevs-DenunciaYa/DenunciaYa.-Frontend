465b2c3c8e71e21161a0b90b23bac78c3e6d4df3
TicSide <66962537+TicSide@users.noreply.github.com>
2025-10-06 22:48:11 -0500

    feat(authentication): Adding Authentiaction Routes

---
+++ src/app/authentication-and-account-management/infrastructure/user-api.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UserApi } from './user-api';
+
+
+describe('UserApi', () => {
+  let service: UserApi;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(UserApi);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
