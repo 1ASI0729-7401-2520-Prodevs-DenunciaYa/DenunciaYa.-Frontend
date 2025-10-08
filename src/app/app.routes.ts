83377965d80730eb900045ca531ca09d6983a527
Gabriel Mamani <u202220659@upc.edu.pe>
2025-10-07 19:25:00 -0500

    feat:update depoendencies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const CommunityPage = () =>
+  import('./community/presentation/community-page/community-page')
+    .then(m => m.CommunityPage);
+
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'communities/home', loadComponent: CommunityPage },
+      { path: '', redirectTo: '/communities/home', pathMatch: 'full' }
+    ]
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import {CommunityPage} from './community/presentation/community-page/community-page';
+import {LayoutComponent} from './shared/presentation/components/layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommunityPage, LayoutComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('denunciaya-frontend');
+  protected readonly title = signal('learning-center');
+  private readonly translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.addLangs(['en', 'es']);
+    this.translate.setDefaultLang('en');
+
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use(browserLang?.match(/en|es/) ? browserLang : 'en');
+  }
 }
